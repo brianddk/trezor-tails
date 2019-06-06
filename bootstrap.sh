f15da2376875fb2c45f9a56dc9ff1adf25dc9a0f
brianddk <brianddk@users.noreply.github.com>
2019-06-06 02:50:09 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 err_report() {
   touch $locksdir/.error
   msg="errexit on line $(caller)"
-  zenity --error --text=$msg
+  zenity --error --text="$msg"
   exit 1
 }
 
@@ -46,13 +46,13 @@ sudo_thread() {
 
   # sudo_second_stage
   msg="Installing TEMPORARY packages, you can choose NOT to persist these packages"
-  zenity --question --text=$msg &
+  zenity --question --text="$msg" &
   
   # Install packages needed for python / pip
   apt-get install python3-dev python3-pip cython3 libusb-1.0-0-dev libudev-dev build-essential python3-wheel
   
   msg="Done installing TEMPORARY packages, choose NOT to persist these packages"
-  zenity --question --text=$msg &
+  zenity --question --text="$msg" &
 
   # sudo_signal_done
   touch $locksdir/.second_stage_done
@@ -101,7 +101,7 @@ export https_proxy=socks5://127.0.0.1:9050
 
 user_first_stage || exit 7
 msg="I need root, please return to terminal and enter password"
-zenity --question --text=$msg || exit 8
+zenity --question --text="$msg" || exit 8
 sudo sudo_thread &
 user_thread &
 user_final_state || exit 9
