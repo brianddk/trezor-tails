0ee5cffacdfc594bd2e1987014ff38a76e7982ae
brianddk <brianddk@users.noreply.github.com>
2019-06-08 21:50:10 -0500

    smarter startup scripts

--- assets/localhost.sh
+++ assets/localhost.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-/bin/rm -f /etc/cron.d/localhost
+export PATH="/usr/local/bin:/usr/local/sbin:/usr/sbin:/usr/bin:/sbin:/bin:/root/bin"
 
-/sbin/iptables -I OUTPUT -p tcp --dport 21325 -j ACCEPT -s localhost -d localhost
-/sbin/iptables -I INPUT -p tcp --dport 21325 -j ACCEPT -s localhost -d localhost
+log="/tmp/iptables.log"
+regex=":90\|:41\|:17\|tor"
+lines=$(iptables -L | grep -e "$regex" | tee "$log" | wc -l)
+if (( $lines < 15 ))
+then
+  echo "------[$(date)]: Waiting on network... Retrying------"
+else
+  iptables -I OUTPUT -p tcp --dport 21325 -j ACCEPT -s localhost -d localhost
+  iptables -I INPUT -p tcp --dport 21325 -j ACCEPT -s localhost -d localhost
+  echo 'user_pref("network.proxy.no_proxies_on", "127.0.0.1");' >> ~amnesia/.tor-browser/profile.default/prefs.js
 
-/bin/echo 'user_pref("network.proxy.no_proxies_on", "127.0.0.1");' >> ~amnesia/.tor-browser/profile.default/prefs.js
+  export DISPLAY=:1
+  export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
+  runuser -c 'notify-send "IPTables enabled @ 127.0.0.1:21325" "Trezor Bridge ready. Restart your browser if open."' amnesia
 
-export DISPLAY=:1
-export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
-/sbin/runuser -c '/usr/bin/notify-send "IPTables enabled @ 127.0.0.1:21325" "Trezor Bridge ready. Restart your browser if open."' amnesia
+  rm -f /etc/cron.d/localhost
+fi
--- assets/swapon.sh
+++ assets/swapon.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+export PATH="/usr/local/bin:/usr/local/sbin:/usr/sbin:/usr/bin:/sbin:/bin:/root/bin"
 persist="/live/persistence/TailsData_unlocked"
+rm -f /etc/cron.d/swapon
 
-/bin/rm -f /etc/cron.d/swapon
-/bin/dd if=/dev/zero of=$persist/swapfile bs=1024 count=$((1024*1024))
-/bin/chmod 0600 $persist/swapfile
-/sbin/mkswap $persist/swapfile
-/sbin/swapon.distrib --verbose $persist/swapfile
+if [ ! -f $persist/swapfile ]
+then
+  dd if=/dev/zero of=$persist/swapfile bs=1024 count=$((1024*1024))
+  chmod 0600 $persist/swapfile
+  mkswap $persist/swapfile
+fi
+
+swapon.distrib --verbose $persist/swapfile
 
 export DISPLAY=:1
 export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
-/sbin/runuser -c '/usr/bin/notify-send "Swap" "Swapfile enabled to optimize performance"' amnesia
+runuser -c 'notify-send "Swap" "Swapfile enabled to optimize performance"' amnesia
