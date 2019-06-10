82531cb875785cb051887025dedee711c48fdd83
brianddk <brianddk@users.noreply.github.com>
2019-06-10 10:27:56 -0500

    more testing

--- assets/localhost.sh
+++ assets/localhost.sh
@@ -2,12 +2,8 @@
 export PATH="/usr/local/bin:/usr/local/sbin:/usr/sbin:/usr/bin:/sbin:/bin:/root/bin"
 
 log="/tmp/iptables.log"
-regex=":90\|:41\|:17\|tor"
-lines=$(iptables -L | grep -e "$regex" | tee "$log" | wc -l)
-if (( $lines < 15 ))
+if grep "Bootstrapped 100%: Done" "/var/log/tor/log"
 then
-  echo "------[$(date)]: Waiting on network... Retrying------"
-else
   iptables -I OUTPUT -p tcp --dport 21325 -j ACCEPT -s localhost -d localhost
   iptables -I INPUT -p tcp --dport 21325 -j ACCEPT -s localhost -d localhost
   echo 'user_pref("network.proxy.no_proxies_on", "127.0.0.1");' >> ~amnesia/.tor-browser/profile.default/prefs.js
@@ -17,4 +13,9 @@ else
   runuser -c 'notify-send "IPTables enabled @ 127.0.0.1:21325" "Trezor Bridge ready. Restart your browser if open."' amnesia
 
   rm -f /etc/cron.d/localhost
+else
+  {
+    grep "Bootstrap" "/var/log/tor/log"
+    echo "------[$(date)]: Waiting on tor... Retrying------"
+  } >> "$log"
 fi
